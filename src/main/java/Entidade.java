b1ea53597e895a80f5f6f9b380af9c3f553be6c1
Jorge7591285568 <adm.unifacs1@hotmail.com>
2024-02-14 18:06:52 +0000

    ReservaCarro

--- src/main/java/Entidade.java
+++ src/main/java/Entidade.java
@@ -1,9 +1,10 @@
 public class Entidade {
 
     public static void main(String[] args) {
+        Carro carro = new Carro("wxy4f34", "Fiat", "Beetle", 2020, "Vermelho", 30, sim);
         Carro carro1 = new Carro("wxy4f32", "Volks", "gol", 2000, "Amarelo", 20, sim);
         Carro carro2 = new Carro("wxy4f33", "Hunday", "hylux", 2010, "Branca", 50, sim);
-        Carro carro = new Carro("wxy4f34", "Fiat", "Beetle", 2020, "Vermelho", 30, sim);
+        
 
         Cliente cliente = new Cliente("jorge", "12345678901", "75999990101", "jorge75999990101@hotmail.com");
         Cliente cliente1 = new Cliente("jorge1", "12345678902", "75999990102", "jorge75999990102@hotmail.com");
