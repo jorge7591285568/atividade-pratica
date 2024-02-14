ad4634548b93e33a6e28aebcb5d6eabf7f78438c
Jorge7591285568 <adm.unifacs1@hotmail.com>
2024-02-14 16:24:10 +0000

    ReservaAuto

---
+++ src/main/java/Carro.java
@@ -0,0 +1,69 @@
+public class Carro {
+    private String placa;
+    private String marca;
+    private String modelo;
+    private int ano;
+    private String cor;
+    private Double diaria;
+    private Boolean disponivel;
+    
+   
+    public Carro(String placa, String marca, String modelo, int ano, String cor, Double diaria, Boolean disponivel) {
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.cor = cor;
+        this.diaria = diaria;
+        this.disponivel = disponivel;
+    }
+    public String getPlaca() {
+        return placa;
+    }
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public int getAno() {
+        return ano;
+    }
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    public Double getDiaria() {
+        return diaria;
+    }
+    public void setDiaria(Double diaria) {
+        this.diaria = diaria;
+    }
+    public Boolean getDisponivel() {
+        return disponivel;
+    }
+    public void setDisponivel(Boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+    
+    @Override
+    public String toString() {
+        return "Carro [placa=" + placa + ", marca=" + marca + ", modelo=" + modelo + ", ano=" + ano + ", cor=" + cor
+                + ", diaria=" + diaria + ", disponivel=" + disponivel + "]";
+    }
+
+}
---
+++ src/main/java/Cliente.java
@@ -0,0 +1,19 @@
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+
+
+    public Cliente(String nome, String cpf, String telefone, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+    }
+    
+    @Override
+    public String toString() {
+        return "Cliente [nome=" + nome + ", cpf=" + cpf + ", telefone=" + telefone + ", email=" + email + "]";
+    }
+}
