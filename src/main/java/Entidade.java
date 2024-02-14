4e86f768afbe0f5f73d3ef4e3d35cba931fd2558
Jorge7591285568 <adm.unifacs1@hotmail.com>
2024-02-14 16:44:15 +0000

    ResevaAuto

---
+++ src/main/java/Entidade.java
@@ -0,0 +1,12 @@
+public class Entidade {
+
+    public static void main(String[] args) {
+        Carro carro1 = new Carro("wxy4f32", "Volks", "gol", 2000, "Amarelo", 20, sim);
+        Carro carro2 = new Carro("wxy4f33", "Hunday", "hylux", 2010, "Branca", 50, sim);
+        Carro carro = new Carro("wxy4f34", "Fiat", "Beetle", 2020, "Vermelho", 30, sim);
+
+        Cliente cliente = new Cliente("jorge", "12345678901", "75999990101", "jorge75999990101@hotmail.com");
+        Cliente cliente1 = new Cliente("jorge1", "12345678902", "75999990102", "jorge75999990102@hotmail.com");
+        Cliente cliente2 = new Cliente("jorge2", "12345678903", "75999990103", "jorge75999990103@hotmail.com");
+    }
+}
